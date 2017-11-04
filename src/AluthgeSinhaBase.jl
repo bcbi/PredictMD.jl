64b6972b3df20de9760cdbfba8d566a492ec6b0e
Dilum Aluthge <dilum@aluthge.com>
2017-11-04 16:14:03 -0400

    Function to generate fake data for testing

--- src/AluthgeSinhaBase.jl
+++ src/AluthgeSinhaBase.jl
@@ -1,6 +1,7 @@
 module AluthgeSinhaBase
 
 # package code goes here
+include("fakedata.jl")
 include("util.jl")
 include("version.jl")
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using AluthgeSinhaBase
 using Base.Test
 
+include("test_fakedata.jl")
 include("test_util.jl")
