2aea6f523ade017c8f779128421da6a0657d237f
Dilum Aluthge <dilum@aluthge.com>
2017-11-23 01:59:48 -0500

    Generate VERSION from VERSION_STRING

--- src/version.jl
+++ src/version.jl
@@ -1,1 +1,8 @@
 const VERSION_STRING = "0.2+dev"
+
+const VERSION = try
+    convert(VersionNumber, strip(VERSION_STRING))
+catch e
+    warn("while creating AluthgeSinhaBase.VERSION, ignoring error $(e)")
+    VersionNumber(0)
+end
---
+++ test/test_version.jl
@@ -0,0 +1,3 @@
+@test( typeof(AluthgeSinhaBase.VERSION_STRING) <: AbstractString)
+
+@test( typeof(AluthgeSinhaBase.VERSION) <: VersionNumber)
