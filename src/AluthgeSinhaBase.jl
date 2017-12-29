1e68b7519192e04a49c0679b3aa81f45ecc9b549
Dilum Aluthge <dilum@aluthge.com>
2017-12-28 19:30:11 -0500

    New file structure

--- src/AluthgeSinhaBase.jl
+++ src/AluthgeSinhaBase.jl
@@ -2,6 +2,20 @@ __precompile__(true)
 
 module AluthgeSinhaBase
 
+include("base.jl")
+include("calibration.jl")
+include("cluster.jl")
+include("datasets.jl")
+include("decomposition.jl")
+include("ensemble.jl")
+include("linearmodel.jl")
+include("modelselection.jl")
+include("multiclass.jl")
+include("multioutput.jl")
+include("neuralnetwork.jl")
+include("pipeline.jl")
+include("preprocessing.jl")
+include("utils.jl")
 include("version.jl")
 
 # # abstract types to export:
