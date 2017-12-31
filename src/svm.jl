7dd53e95535c2ebeee849b28530a1bc3fcb73046
Dilum Aluthge <dilum@aluthge.com>
2017-12-30 21:45:36 -0500

    Add Cohen's Kappa to table of binary classifier metrics

---
+++ src/svm.jl
@@ -0,0 +1,1 @@
+include("svm/libsvm.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,6 @@ const asb = AluthgeSinhaBase
 import Base.Test
 
 include("test_base.jl")
-include("test_metrics/")
+include("test_metrics.jl")
 
 include("runexamples.jl")
