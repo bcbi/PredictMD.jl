38f22a72e1cef017da3cf9656de426d8d30e24db
Dilum Aluthge <dilum@aluthge.com>
2018-01-11 13:19:14 -0500

    Changing tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,20 @@
-import AluthgeSinhaBase
-const asb = AluthgeSinhaBase
-import Base.Test
+# import AluthgeSinhaBase
+# const asb = AluthgeSinhaBase
+# import Base.Test
+#
+# include("test_base.jl")
+# include("test_metrics.jl")
+#
+# include("runexamples.jl")
 
-include("test_base.jl")
-include("test_metrics.jl")
 
-include("runexamples.jl")
+
+# ENV["LOADTRAINEDMODELSFROMFILE"] = "false"
+# ENV["SAVETRAINEDMODELSTOFILE"] = "true"
+# include("../examples/bostonhousing.jl")
+# include("../examples/breastcancerbiopsy.jl")
+#
+# ENV["LOADTRAINEDMODELSFROMFILE"] = "true"
+# ENV["SAVETRAINEDMODELSTOFILE"] = "false"
+# include("../examples/bostonhousing.jl")
+# include("../examples/breastcancerbiopsy.jl")
