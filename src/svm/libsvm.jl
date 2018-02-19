8e70198c53299256f2882ea397ac34dcafad217d
Dilum Aluthge <dilum@aluthge.com>
2018-02-18 21:21:11 -0500

    Change another warning to error

--- src/svm/libsvm.jl
+++ src/svm/libsvm.jl
@@ -180,7 +180,7 @@ function predict(
                 mean(abs, differences),
                 "."
                 )
-            warn(msg)
+            error(msg)
         end
         result = convert(Vector, vec(predictedvalues))
         return result
