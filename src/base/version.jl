8c26803f44ee4521da2ec1145d428b84b64a8f9e
Dilum Aluthge <dilum@aluthge.com>
2018-04-09 23:26:25 -0400

    Bump version number from "v0.10.0-DEV" to "v0.10.0"

--- src/base/version.jl
+++ src/base/version.jl
@@ -1,5 +1,5 @@
 const VERSION = try
-    convert(VersionNumber, "v0.10.0-DEV")
+    convert(VersionNumber, "v0.10.0")
 catch e
     warn("while creating AluthgeSinhaBase.VERSION, ignoring error $(e)")
     VersionNumber(0)
