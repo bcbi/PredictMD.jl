2ac559e89a92d21eda1c3630010cd0deb58ca6d9
Dilum Aluthge <dilum@aluthge.com>
2018-04-12 20:47:42 -0400

    Improve tikzpicture handling

--- src/utils/tikzpictures.jl
+++ src/utils/tikzpictures.jl
@@ -1,7 +1,43 @@
 import TikzPictures
 
-function savesvg(tp::TikzPictures.TikzPicture, fn::AbstractString)
-    result = TikzPictures.save(TikzPictures.SVG(fn), tp)
+function save(tp::TikzPictures.TikzPicture, filename::AbstractString)
+    extension = splitext(filename)[2]
+    if extension == ".pdf"
+        return save_pdf(tp, filename)
+    elseif extension == ".tex"
+        return save_tex(tp, filename)
+    elseif extension == ".tikz"
+        return save_tikz(tp, filename)
+    elseif extension == ".svg"
+        return save_svg(tp, filename)
+    else
+        error(
+            string(
+                "I don't know how to save a picture in file format \"",
+                extension,
+                "\".",
+                )
+            )
+    end
+end
+
+function save_pdf(tp::TikzPictures.TikzPicture, filename::AbstractString)
+    result = TikzPictures.save(TikzPictures.PDF(filename), tp)
+    return result
+end
+
+function save_tex(tp::TikzPictures.TikzPicture, filename::AbstractString)
+    result = TikzPictures.save(TikzPictures.TEX(filename), tp)
+    return result
+end
+
+function save_tikz(tp::TikzPictures.TikzPicture, filename::AbstractString)
+    result = TikzPictures.save(TikzPictures.TIKZ(filename), tp)
+    return result
+end
+
+function save_svg(tp::TikzPictures.TikzPicture, filename::AbstractString)
+    result = TikzPictures.save(TikzPictures.SVG(filename), tp)
     return result
 end
 
@@ -11,8 +47,8 @@ function open(tp::TikzPictures.TikzPicture)
     return result
 end
 
-function open(tp::TikzPictures.TikzPicture, fn::AbstractString)
-    saveresult = savesvg(tp, fn)
-    openresult = open(fn)
+function open(tp::TikzPictures.TikzPicture, filename::AbstractString)
+    saveresult = savesvg(tp, filename)
+    openresult = open(filename)
     return openresult
 end
