467bcf546ee9a8c2c74c74a68f2734320724bfea
Dilum Aluthge <dilum@aluthge.com>
2018-05-16 04:16:48 -0400

    Restore type annotation

--- src/io/saveload.jl
+++ src/io/saveload.jl
@@ -3,8 +3,7 @@ import FileIO
 import JLD2
 import ProgressMeter
 
-# function save_model(filename::AbstractString, fittable_object_to_save::Fittable)
-function save_model(filename::AbstractString, fittable_object_to_save)
+function save_model(filename::AbstractString, fittable_object_to_save::Fittable)
     # make sure that the filename ends in ".jld2"
     if lowercase(strip(splitext(filename)[2])) != ".jld2"
         error(
