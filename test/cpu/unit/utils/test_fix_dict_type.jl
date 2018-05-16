71026a220d16d4215f1cef069f416ccc3fde5108
Dilum Aluthge <dilum@aluthge.com>
2018-05-16 02:28:20 -0400

    fix tests

--- test/cpu/unit/utils/test_fix_dict_type.jl
+++ test/cpu/unit/utils/test_fix_dict_type.jl
@@ -6,11 +6,11 @@ dict_1[Symbol(:y)] = Float64(2.2)
 dict_1[Symbol(:z)] = Float64(3.3)
 Base.Test.@test(typeof(dict_1) <: Dict{Any, Any})
 
-dict_2 = fix_dict_type(dict_1)
+dict_2 = PredictMD.fix_dict_type(dict_1)
 Base.Test.@test(typeof(dict_1_fixed) <: Dict{Symbol, Float64})
 
-dict_3 = fix_dict_type(dict_2)
+dict_3 = PredictMD.fix_dict_type(dict_2)
 Base.Test.@test(typeof(dict_3) <: Dict{Symbol, Float64})
 
-dict_4 = fix_dict_type(dict_3)
+dict_4 = PredictMD.fix_dict_type(dict_3)
 Base.Test.@test(typeof(dict_4) <: Dict{Symbol, Float64})
--- test/cpu/unit/utils/test_fix_vector_type.jl
+++ test/cpu/unit/utils/test_fix_vector_type.jl
@@ -6,11 +6,11 @@ push!(vector_1, Float64(2.0))
 push!(vector_1, Float64(3.0))
 Base.Test.@test(typeof(vector_1) <: Vector{Any})
 
-vector_2 = fix_vector_type(vector_1)
+vector_2 = PredictMD.fix_vector_type(vector_1)
 Base.Test.@test(typeof(vector_2) <: Vector{Float64})
 
-vector_3 = fix_vector_type(vector_2)
+vector_3 = PredictMD.fix_vector_type(vector_2)
 Base.Test.@test(typeof(vector_3) <: Vector{Float64})
 
-vector_4 = fix_vector_type(vector_3)
+vector_4 = PredictMD.fix_vector_type(vector_3)
 Base.Test.@test(typeof(vector_4) <: Vector{Float64})
