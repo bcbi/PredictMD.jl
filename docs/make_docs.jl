dcda906774d9e3cef884fe266e9e4941caa14dd9
Dilum Aluthge <dilum@aluthge.com>
2018-05-21 17:51:28 -0400

    Progress commit

--- docs/make_docs.jl
+++ docs/make_docs.jl
@@ -2,6 +2,46 @@ import Documenter
 import Literate
 import PredictMD
 
+examples_input_directory = joinpath(
+    @__DIR__,
+    "..",
+    "examples",
+    )
+
+examples_output_directory = joinpath(
+    @__DIR__,
+    "",
+    "",
+    "",
+    "",
+    )
+
+Literate.markdown(
+    ,
+    examples_output_directory,
+    )
+Literate.notebook(
+    ,
+    examples_output_directory,
+    )
+Literate.script(
+    ,
+    examples_output_directory,
+    )
+
+Literate.markdown(
+    ,
+    examples_output_directory,
+    )
+Literate.notebook(
+    ,
+    examples_output_directory,
+    )
+Literate.script(
+    ,
+    examples_output_directory,
+    )
+
 Documenter.makedocs(
     modules = [PredictMD],
     sitename = "PredictMD.jl",
--- test/runtests.jl
+++ test/runtests.jl
@@ -12,34 +12,27 @@ println(string("PredictMD Version ", PredictMD.VERSION))
 ENV["PREDICTMD_RUNTESTS"] = "true"
 
 Base.Test.@testset "PredictMD test suite" begin
-    #
     Base.Test.@testset "Unit tests (CPU)" begin
         info("INFO Running unit tests (CPU)")
-        #
         Base.Test.@testset "base" begin
             include("cpu/unit/base/test_version.jl")
         end
-        #
         Base.Test.@testset "metrics" begin
             include("cpu/unit/metrics/test_coefficientofdetermination.jl")
             include("cpu/unit/metrics/test_cohenkappa.jl")
         end
-        #
         Base.Test.@testset "utils" begin
             include("cpu/unit/utils/test_fix_dict_type.jl")
             include("cpu/unit/utils/test_fix_vector_type.jl")
         end
     end
-    #
     Base.Test.@testset "Test examples (CPU)" begin
         info("INFO testing examples (CPU)")
-        #
         Base.Test.@testset "Boston housing regression" begin
-            include("")
+            # include("")
         end
-        #
         Base.Test.@testset "Breast cancer biopsy classification" begin
-            include("")
+            # include("")
         end
     end
 end
