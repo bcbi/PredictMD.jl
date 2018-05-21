a8038e54e18abb88a33a68b79bec01490b7ba77d
Dilum Aluthge <dilum@aluthge.com>
2018-05-21 17:24:18 -0400

    Progress commit

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,28 +12,34 @@ println(string("PredictMD Version ", PredictMD.VERSION))
 ENV["PREDICTMD_RUNTESTS"] = "true"
 
 Base.Test.@testset "PredictMD test suite" begin
+    #
     Base.Test.@testset "Unit tests (CPU)" begin
         info("INFO Running unit tests (CPU)")
+        #
         Base.Test.@testset "base" begin
             include("cpu/unit/base/test_version.jl")
         end
+        #
         Base.Test.@testset "metrics" begin
             include("cpu/unit/metrics/test_coefficientofdetermination.jl")
             include("cpu/unit/metrics/test_cohenkappa.jl")
         end
+        #
         Base.Test.@testset "utils" begin
             include("cpu/unit/utils/test_fix_dict_type.jl")
             include("cpu/unit/utils/test_fix_vector_type.jl")
         end
     end
-    include("../docs/test_examples.jl")
-    # Base.Test.@testset "Functional tests (CPU)" begin
-    #     info("INFO Running functional tests (CPU)")
-    #     Base.Test.@testset "Boston housing regression" begin
-    #         include("cpu/functional/bostonhousing/setup_bostonhousing.jl")
-    #     end
-    #     Base.Test.@testset "Breast cancer biopsy classification" begin
-    #         include("cpu/functional/breastcancerbiopsy/setup_breastcancerbiopsy.jl")
-    #     end
-    # end
+    #
+    Base.Test.@testset "Test examples (CPU)" begin
+        info("INFO testing examples (CPU)")
+        #
+        Base.Test.@testset "Boston housing regression" begin
+            include("")
+        end
+        #
+        Base.Test.@testset "Breast cancer biopsy classification" begin
+            include("")
+        end
+    end
 end
