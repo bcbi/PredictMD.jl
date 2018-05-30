eee5ce8faa75a4718f6fb99347a2bfa168862e54
Dilum Aluthge <dilum@aluthge.com>
2018-05-30 00:04:54 -0400

    Refactor code: use execute_notebooks variable

--- docs/make_examples.jl
+++ docs/make_examples.jl
@@ -3,6 +3,8 @@ import PredictMD
 
 ENV["PREDICTMD_IS_MAKE_EXAMPLES"] = "true"
 
+execute_notebooks = true
+
 examples_input_parent_directory = joinpath(
     @__DIR__,
     "..",
@@ -44,7 +46,7 @@ Literate.notebook(
     joinpath(boston_housing_input_directory, "01_preprocess_data.jl"),
     boston_housing_output_directory;
     documenter = true,
-    execute = true,
+    execute = execute_notebooks,
     )
 Literate.script(
     joinpath(boston_housing_input_directory, "01_preprocess_data.jl"),
@@ -62,7 +64,7 @@ Literate.notebook(
     joinpath(boston_housing_input_directory, "02_linear_regression.jl"),
     boston_housing_output_directory;
     documenter = true,
-    execute = true,
+    execute = execute_notebooks,
     )
 Literate.script(
     joinpath(boston_housing_input_directory, "02_linear_regression.jl"),
@@ -80,7 +82,7 @@ Literate.notebook(
     joinpath(boston_housing_input_directory, "03_random_forest_regression.jl"),
     boston_housing_output_directory;
     documenter = true,
-    execute = true,
+    execute = execute_notebooks,
     )
 Literate.script(
     joinpath(boston_housing_input_directory, "03_random_forest_regression.jl"),
@@ -98,7 +100,7 @@ Literate.notebook(
     joinpath(boston_housing_input_directory, "04_knet_mlp_regression.jl"),
     boston_housing_output_directory;
     documenter = true,
-    execute = true,
+    execute = execute_notebooks,
     )
 Literate.script(
     joinpath(boston_housing_input_directory, "04_knet_mlp_regression.jl"),
@@ -116,7 +118,7 @@ Literate.notebook(
     joinpath(boston_housing_input_directory, "05_compare_models.jl"),
     boston_housing_output_directory;
     documenter = true,
-    execute = true,
+    execute = execute_notebooks,
     )
 Literate.script(
     joinpath(boston_housing_input_directory, "05_compare_models.jl"),
@@ -134,7 +136,7 @@ Literate.notebook(
     joinpath(boston_housing_input_directory, "06_get_model_output.jl"),
     boston_housing_output_directory;
     documenter = true,
-    execute = true,
+    execute = execute_notebooks,
     )
 Literate.script(
     joinpath(boston_housing_input_directory, "06_get_model_output.jl"),
@@ -162,7 +164,7 @@ Literate.notebook(
     joinpath(breast_cancer_biopsy_input_directory, "01_preprocess_data.jl"),
     breast_cancer_biopsy_output_directory;
     documenter = true,
-    execute = true,
+    execute = execute_notebooks,
     )
 Literate.script(
     joinpath(breast_cancer_biopsy_input_directory, "01_preprocess_data.jl"),
@@ -180,7 +182,7 @@ Literate.notebook(
     joinpath(breast_cancer_biopsy_input_directory, "02_smote.jl"),
     breast_cancer_biopsy_output_directory;
     documenter = true,
-    execute = true,
+    execute = execute_notebooks,
     )
 Literate.script(
     joinpath(breast_cancer_biopsy_input_directory, "02_smote.jl"),
@@ -198,7 +200,7 @@ Literate.notebook(
     joinpath(breast_cancer_biopsy_input_directory, "03_logistic_classifier.jl"),
     breast_cancer_biopsy_output_directory;
     documenter = true,
-    execute = true,
+    execute = execute_notebooks,
     )
 Literate.script(
     joinpath(breast_cancer_biopsy_input_directory, "03_logistic_classifier.jl"),
@@ -216,7 +218,7 @@ Literate.notebook(
     joinpath(breast_cancer_biopsy_input_directory, "04_random_forest_classifier.jl"),
     breast_cancer_biopsy_output_directory;
     documenter = true,
-    execute = true,
+    execute = execute_notebooks,
     )
 Literate.script(
     joinpath(breast_cancer_biopsy_input_directory, "04_random_forest_classifier.jl"),
@@ -234,7 +236,7 @@ Literate.notebook(
     joinpath(breast_cancer_biopsy_input_directory, "05_c_svc_svm_classifier.jl"),
     breast_cancer_biopsy_output_directory;
     documenter = true,
-    execute = true,
+    execute = execute_notebooks,
     )
 Literate.script(
     joinpath(breast_cancer_biopsy_input_directory, "05_c_svc_svm_classifier.jl"),
@@ -252,7 +254,7 @@ Literate.notebook(
     joinpath(breast_cancer_biopsy_input_directory, "06_nu_svc_svm_classifier.jl"),
     breast_cancer_biopsy_output_directory;
     documenter = true,
-    execute = true,
+    execute = execute_notebooks,
     )
 Literate.script(
     joinpath(breast_cancer_biopsy_input_directory, "06_nu_svc_svm_classifier.jl"),
@@ -270,7 +272,7 @@ Literate.notebook(
     joinpath(breast_cancer_biopsy_input_directory, "07_knet_mlp_classifier.jl"),
     breast_cancer_biopsy_output_directory;
     documenter = true,
-    execute = true,
+    execute = execute_notebooks,
     )
 Literate.script(
     joinpath(breast_cancer_biopsy_input_directory, "07_knet_mlp_classifier.jl"),
@@ -288,7 +290,7 @@ Literate.notebook(
     joinpath(breast_cancer_biopsy_input_directory, "08_compare_models.jl"),
     breast_cancer_biopsy_output_directory;
     documenter = true,
-    execute = true,
+    execute = execute_notebooks,
     )
 Literate.script(
     joinpath(breast_cancer_biopsy_input_directory, "08_compare_models.jl"),
@@ -306,7 +308,7 @@ Literate.notebook(
     joinpath(breast_cancer_biopsy_input_directory, "09_get_model_output.jl"),
     breast_cancer_biopsy_output_directory;
     documenter = true,
-    execute = true,
+    execute = execute_notebooks,
     )
 Literate.script(
     joinpath(breast_cancer_biopsy_input_directory, "09_get_model_output.jl"),
