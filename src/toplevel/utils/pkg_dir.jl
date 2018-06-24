9fca5ec33ee24dcb0572a20aaafff271480d71ee
Dilum Aluthge <dilum@aluthge.com>
2018-06-23 21:49:51 -0400

    Progress commit

--- src/toplevel/utils/pkg_dir.jl
+++ src/toplevel/utils/pkg_dir.jl
@@ -1,3 +1,17 @@
 """
 """
-dir(path...) = joinpath(dirname(dirname(dirname(dirname(@__FILE__)))),path...)
+function dir(parts...)
+    predictmd_root_dir = dirname( # PredictMD/
+        dirname( # PredictMD/src/
+            dirname( # PredictMD/src/./
+                dirname( # PredictMD/src/./toplevel/
+                    dirname( # PredictMD/src/./toplevel/utils/
+                        @__FILE__ # PredictMD/src/./toplevel/utils/pkg_dir.jl
+                        )
+                    )
+                )
+            )
+        )
+    result = joinpath(predictmd_root_dir, parts...)
+    return result
+end
