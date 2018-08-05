be8274709275dbc7b767548ff62688ae169a05e8
Dilum Aluthge <dilum@aluthge.com>
2018-08-04 21:07:59 -0400

    Backup commit

--- src/submodules/Compilation/Compilation.jl
+++ src/submodules/Compilation/Compilation.jl
@@ -8,8 +8,11 @@ module Compilation # begin submodule PredictMD.Compilation
 # PredictMD.Compilation source files #######################################
 ############################################################################
 
-# submodules/Server/SUBFOLDER/
-include(joinpath(".", "SUBFOLDER", "FILENAME.jl",))
+# submodules/Server/buildexecutable/
+include(joinpath(".", "buildexecutable", "buildexecutable.jl",))
+
+# submodules/Server/packagecompiler/
+include(joinpath(".", "packagecompiler", "packagecompiler.jl",))
 
 end # end submodule PredictMD.Compilation
 
