030b6d578e9c96961458ab8057fbbe6539646146
Dilum Aluthge <dilum@aluthge.com>
2018-08-04 21:12:31 -0400

    Backup commit

--- src/submodules/Server/Server.jl
+++ src/submodules/Server/Server.jl
@@ -9,12 +9,14 @@ module Server # begin submodule PredictMD.Server
 ############################################################################
 
 # submodules/Server/cryptography/
+include(joinpath(".", "cryptography", "mbedtls.jl",))
 include(joinpath(".", "cryptography", "gnutls.jl",))
 include(joinpath(".", "cryptography", "transportlayersecurity.jl",))
 
 # submodules/Server/web/
 include(joinpath(".", "web", "genie.jl",))
 include(joinpath(".", "web", "http.jl",))
+include(joinpath(".", "web", "httpclient.jl",))
 include(joinpath(".", "web", "httpserver.jl",))
 include(joinpath(".", "web", "juliawebapi.jl",))
 include(joinpath(".", "web", "mux.jl",))
