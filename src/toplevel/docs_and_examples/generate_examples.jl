2f8af7bc6920e8777ad6105fdd942604010ce1d1
Dilum Aluthge <dilum@aluthge.com>
2018-08-12 22:30:06 -0400

    Move "examples/" to "templates/examples/"

--- src/toplevel/docs_and_examples/generate_examples.jl
+++ src/toplevel/docs_and_examples/generate_examples.jl
@@ -60,7 +60,7 @@ function generate_examples(
     mkpath(temp_examples_dir)
 
     examples_input_parent_directory =
-        PredictMD.pkg_dir("examples")
+        PredictMD.pkg_dir("templates", "examples")
 
     cpu_examples_input_parent_directory = joinpath(
         examples_input_parent_directory,
