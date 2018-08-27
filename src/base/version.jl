81842aaf3fb3b4a2b95c5f4f546896ab0fdc667c
Dilum Aluthge <dilum@aluthge.com>
2018-08-26 23:06:29 -0400

    Bump version number from "v0.19.0-DEV" to "v0.19.0"

--- src/base/version.jl
+++ src/base/version.jl
@@ -1,7 +1,7 @@
 ##### Beginning of file
 
 const PREDICTMD_VERSION = try
-    convert(VersionNumber, "v0.19.0-DEV")
+    convert(VersionNumber, "v0.19.0")
 catch e
     Compat.@warn("While creating PredictMD.PREDICTMD_VERSION, ignoring error $(e)")
     VersionNumber(0)
