25c2f5fb13b4ebb893b0e37779b788540cf47ad5
Dilum Aluthge <dilum@aluthge.com>
2019-01-05 22:12:44 -0500

    Attempt to fix tests and improve coverage

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -4,41 +4,46 @@
 
 set -ev
 
+echo "COMPILED_MODULES=$COMPILED_MODULES"
+
+export JULIA_FLAGS="--check-bounds=yes --code-coverage=all --color=yes --compiled-modules=$COMPILED_MODULES --inline=no"
+echo "JULIA_FLAGS=$JULIA_FLAGS"
+
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     export PATH=/Library/TeX/texbin:"$PATH"
 fi
 
-julia --check-bounds=yes --color=yes -e '
+julia $JULIA_FLAGS -e '
     import Pkg;
     Pkg.build("PredictMD");
     '
 
-julia --check-bounds=yes --color=yes -e '
+julia $JULIA_FLAGS -e '
     import PredictMD;
     '
 
-julia --check-bounds=yes --color=yes -e '
+julia $JULIA_FLAGS -e '
     import Pkg;
     Pkg.test("PredictMD"; coverage=true);
     '
 
-julia --check-bounds=yes --color=yes -e '
+julia $JULIA_FLAGS -e '
     import Pkg;
     Pkg.add("Coverage");
     '
 
-julia --check-bounds=yes --color=yes -e '
+julia $JULIA_FLAGS -e '
     import Pkg;
     cd(Pkg.dir("PredictMD"));
     import Coverage;
     Coverage.Codecov.submit(Coverage.Codecov.process_folder());
     '
 
-julia --check-bounds=yes --color=yes -e '
+julia $JULIA_FLAGS -e '
     import Pkg;
     include(joinpath(Pkg.dir("PredictMD"), "docs", "make.jl",));
     '
-    
+
 cat Project.toml
 
 cat Manifest.toml
