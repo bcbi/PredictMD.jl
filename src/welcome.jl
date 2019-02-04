6b2d7c7b95f65e6fc23d75aef383743c7937a130
Dilum Aluthge <dilum@aluthge.com>
2019-02-03 22:11:34 -0500

    Remove "www"

--- src/welcome.jl
+++ src/welcome.jl
@@ -4,7 +4,7 @@ function _print_welcome_message()::Nothing
     predictmd_version::VersionNumber = version()
     predictmd_pkgdir::String = package_directory()
     @info(string("This is PredictMD, version ",predictmd_version,),)
-    @info(string("For help, please visit https://www.predictmd.net",),)
+    @info(string("For help, please visit https://predictmd.net",),)
     @debug(string("PredictMD package directory: ",predictmd_pkgdir,),)
     return nothing
 end
