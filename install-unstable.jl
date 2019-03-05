bcd4d97cc532ab453201a9d2f9df38787ab3c04a
Dilum Aluthge <dilum@aluthge.com>
2019-03-04 19:38:51 -0500

    Update install-unstable.jl

--- install-unstable.jl
+++ install-unstable.jl
@@ -1,19 +1,20 @@
 import Pkg; 
-Pkg.Registry.update(); 
-Pkg.update(); 
-Pkg.Registry.add(Pkg.RegistrySpec(url="https://github.com/JuliaRegistries/General")); 
-Pkg.Registry.update(); 
-Pkg.update(); 
+
 Pkg.add(Pkg.PackageSpec(url="https://github.com/bcbi/PredictMD.jl", rev="develop")); 
 Pkg.build("PredictMD"); 
+
 Pkg.add(Pkg.PackageSpec(url="https://github.com/bcbi/PredictMDExtra.jl", rev="develop")); 
 Pkg.build("PredictMDExtra"); 
+
 Pkg.add(Pkg.PackageSpec(url="https://github.com/bcbi/PredictMDFull.jl", rev="develop")); 
 Pkg.build("PredictMDFull"); 
+
 ENV["JULIA_DEBUG"] = "all"; 
+
 import PredictMD; 
 import PredictMDExtra; 
 import PredictMDFull; 
-println("PredictMD (unstable develop branch) was installed successfully.");
-println("You are now ready to use PredictMD.");
-println("For help, visit https://predictmd.net");
+
+println("PredictMD (unstable develop branch) was installed successfully."); 
+println("You are now ready to use PredictMD."); 
+println("For help, visit https://predictmd.net"); 
