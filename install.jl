a6bc92d594182e7bd993e48bcacbfe6a8942256a
Dilum Aluthge <dilum@aluthge.com>
2019-03-04 19:38:54 -0500

    Update install.jl

--- install.jl
+++ install.jl
@@ -1,19 +1,28 @@
 import Pkg; 
-Pkg.Registry.update(); 
-Pkg.update(); 
-Pkg.Registry.add(Pkg.RegistrySpec(url="https://github.com/JuliaRegistries/General")); 
-Pkg.Registry.update(); 
-Pkg.update(); 
-Pkg.add(Pkg.PackageSpec(url="https://github.com/bcbi/PredictMD.jl", rev="master")); 
+
+Pkg.Registry.add(Pkg.RegistrySpec(url="https://github.com/bcbi/PredictMDRegistry.git")); 
+
+Pkg.Registry.update(Pkg.RegistrySpec(uuid="26a550a3-39fe-4af4-af6d-e8814c2b6dd9")); 
+
+Pkg.add("PredictMD"); 
 Pkg.build("PredictMD"); 
-Pkg.add(Pkg.PackageSpec(url="https://github.com/bcbi/PredictMDExtra.jl", rev="master")); 
+
+Pkg.add("PredictMDExtra"); 
 Pkg.build("PredictMDExtra"); 
-Pkg.add(Pkg.PackageSpec(url="https://github.com/bcbi/PredictMDFull.jl", rev="master")); 
+
+Pkg.add("PredictMDFull"); 
 Pkg.build("PredictMDFull"); 
+
+Pkg.build("PredictMD"); 
+Pkg.build("PredictMDExtra"); 
+Pkg.build("PredictMDFull"); 
+
 ENV["JULIA_DEBUG"] = "all"; 
+
 import PredictMD; 
 import PredictMDExtra; 
 import PredictMDFull; 
-println("PredictMD was installed successfully.");
-println("You are now ready to use PredictMD.");
-println("For help, visit https://predictmd.net");
+
+println("PredictMD was installed successfully."); 
+println("You are now ready to use PredictMD."); 
+println("For help, visit https://predictmd.net"); 
