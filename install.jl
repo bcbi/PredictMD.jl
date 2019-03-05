0198cc8b75284eff9f191048ec3de6de69a71777
Dilum Aluthge <dilum@aluthge.com>
2019-03-04 20:12:34 -0500

    Update install.jl

--- install.jl
+++ install.jl
@@ -1,7 +1,7 @@
 import Pkg; 
 
-Pkg.Registry.add(Pkg.RegistrySpec(url="https://github.com/bcbi/PredictMDRegistry.git")); 
-Pkg.Registry.update(Pkg.RegistrySpec(uuid="26a550a3-39fe-4af4-af6d-e8814c2b6dd9")); 
+Pkg.Registry.add(Pkg.RegistrySpec(url="https://github.com/bcbi/PredictMDRegistry.git",name="PredictMDRegistry",uuid="26a550a3-39fe-4af4-af6d-e8814c2b6dd9",)); 
+Pkg.Registry.update(Pkg.RegistrySpec(name="PredictMDRegistry",uuid="26a550a3-39fe-4af4-af6d-e8814c2b6dd9")); 
 
 Pkg.add("PredictMD"); 
 Pkg.build("PredictMD"); 
