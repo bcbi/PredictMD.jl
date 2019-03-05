b27b714dcb34e6b2cd972d786f8b14b19eed0e13
Dilum Aluthge <dilum@aluthge.com>
2019-03-04 20:22:23 -0500

    Update install-unstable.jl

--- install-unstable.jl
+++ install-unstable.jl
@@ -1,12 +1,12 @@
 import Pkg; 
 
-Pkg.add(Pkg.PackageSpec(url="https://github.com/bcbi/PredictMD.jl", rev="develop")); 
+Pkg.add(Pkg.PackageSpec(name="PredictMD",rev="develop",url="https://github.com/bcbi/PredictMD.jl",uuid="3e7d7328-36f8-4388-bd01-4613c92c7370",)); 
 Pkg.build("PredictMD"); 
 
-Pkg.add(Pkg.PackageSpec(url="https://github.com/bcbi/PredictMDExtra.jl", rev="develop")); 
+Pkg.add(Pkg.PackageSpec(name="PredictMDExtra",rev="develop",url="https://github.com/bcbi/PredictMDExtra.jl",uuid="d14d998a-9e6b-11e8-16d3-6f2879ea456d",)); 
 Pkg.build("PredictMDExtra"); 
 
-Pkg.add(Pkg.PackageSpec(url="https://github.com/bcbi/PredictMDFull.jl", rev="develop")); 
+Pkg.add(Pkg.PackageSpec(name="PredictMDFull",rev="develop",url="https://github.com/bcbi/PredictMDFull.jl",uuid="5c0c5c38-9dd5-11e8-3ab7-453bd9ce6c97",)); 
 Pkg.build("PredictMDFull"); 
 
 ENV["JULIA_DEBUG"] = "all"; 
