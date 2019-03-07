bd60f88f08992c1fb00c5b6dbe03aadd8619f5ac
Dilum Aluthge <dilum@aluthge.com>
2019-03-06 23:39:02 -0500

    Update install-unstable.jl

--- install-unstable.jl
+++ install-unstable.jl
@@ -1,24 +1,23 @@
 import Pkg; 
 
 try
-    Pkg.Registry.add(Pkg.RegistrySpec(name="General",)); 
+    Pkg.Registry.add("General"); 
 catch e
     @info("ignoring exception: ", e,)
 end
 
 try
-    Pkg.Registry.update(Pkg.RegistrySpec(name="General",)); 
+    Pkg.Registry.update("General"); 
 catch e
     @info("ignoring exception: ", e,)
 end
 
 Pkg.add(Pkg.PackageSpec(name="PredictMD",rev="develop",url="https://github.com/bcbi/PredictMD.jl",uuid="3e7d7328-36f8-4388-bd01-4613c92c7370",)); 
-Pkg.build("PredictMD"); 
-
 Pkg.add(Pkg.PackageSpec(name="PredictMDExtra",rev="develop",url="https://github.com/bcbi/PredictMDExtra.jl",uuid="d14d998a-9e6b-11e8-16d3-6f2879ea456d",)); 
-Pkg.build("PredictMDExtra"); 
-
 Pkg.add(Pkg.PackageSpec(name="PredictMDFull",rev="develop",url="https://github.com/bcbi/PredictMDFull.jl",uuid="5c0c5c38-9dd5-11e8-3ab7-453bd9ce6c97",)); 
+
+Pkg.build("PredictMD"); 
+Pkg.build("PredictMDExtra"); 
 Pkg.build("PredictMDFull"); 
 
 ENV["JULIA_DEBUG"] = "all"; 
