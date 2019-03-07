e1f3bce306c682247ed48b427154df81d3aa6811
Dilum Aluthge <dilum@aluthge.com>
2019-03-06 23:38:20 -0500

    Update install.jl

--- install.jl
+++ install.jl
@@ -3,7 +3,7 @@ import Pkg;
 Pkg.Registry.add(Pkg.RegistrySpec(name="PredictMDRegistry",url="https://github.com/bcbi/PredictMDRegistry.git",uuid="26a550a3-39fe-4af4-af6d-e8814c2b6dd9",)); 
 
 try
-    Pkg.Registry.add(Pkg.RegistrySpec(name="General",)); 
+    Pkg.Registry.add("General"); 
 catch e
     @info("ignoring exception: ", e,)
 end
@@ -11,7 +11,7 @@ end
 Pkg.Registry.update(Pkg.RegistrySpec(name="PredictMDRegistry",uuid="26a550a3-39fe-4af4-af6d-e8814c2b6dd9")); 
 
 try
-    Pkg.Registry.update(Pkg.RegistrySpec(name="General",)); 
+    Pkg.Registry.update("General"); 
 catch e
     @info("ignoring exception: ", e,)
 end
