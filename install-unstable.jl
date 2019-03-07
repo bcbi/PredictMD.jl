1e5317883bf9a59b3ac57302c72a6b04e665fd9d
Dilum Aluthge <dilum@aluthge.com>
2019-03-06 23:44:05 -0500

    Update install-unstable.jl

--- install-unstable.jl
+++ install-unstable.jl
@@ -1,5 +1,8 @@
 import Pkg; 
 
+Pkg.Registry.add(Pkg.RegistrySpec(name="PredictMDRegistry",url="https://github.com/bcbi/PredictMDRegistry.git",uuid="26a550a3-39fe-4af4-af6d-e8814c2b6dd9",)); 
+Pkg.Registry.update(Pkg.RegistrySpec(name="PredictMDRegistry",uuid="26a550a3-39fe-4af4-af6d-e8814c2b6dd9")); 
+
 try
     Pkg.Registry.add("General"); 
 catch e
