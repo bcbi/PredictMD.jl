594f5e8bad01262b71da90b4576c4f9dc7f02e60
Dilum Aluthge <dilum@aluthge.com>
2019-03-06 23:40:00 -0500

    Create registry-no-general.jl

---
+++ registry-no-general.jl
@@ -0,0 +1,3 @@
+import Pkg; 
+Pkg.Registry.add(Pkg.RegistrySpec(url="https://github.com/bcbi/PredictMDRegistry.git")); 
+Pkg.Registry.update(Pkg.RegistrySpec(uuid="26a550a3-39fe-4af4-af6d-e8814c2b6dd9")); 
