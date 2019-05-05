ef7a5bfee938bef65984e6f4e220617df42351c5
Dilum Aluthge <dilum@aluthge.com>
2019-05-05 03:51:33 -0400

    Update Server.jl

--- src/submodules/Server/Server.jl
+++ src/submodules/Server/Server.jl
@@ -9,16 +9,16 @@ module Server # begin submodule PredictMD.Server
 ############################################################################
 
 # submodules/Server/cryptography/
-include(joinpath("cryptography", "mbedtls.jl",))
-include(joinpath("cryptography", "transportlayersecurity.jl",))
+# include(joinpath("cryptography", "mbedtls.jl",))
+# include(joinpath("cryptography", "transportlayersecurity.jl",))
 
 # submodules/Server/web/
-include(joinpath("web", "genie.jl",))
+# include(joinpath("web", "genie.jl",))
 include(joinpath("web", "http.jl",))
-include(joinpath("web", "httpclient.jl",))
-include(joinpath("web", "httpserver.jl",))
-include(joinpath("web", "juliawebapi.jl",))
-include(joinpath("web", "mux.jl",))
+# include(joinpath("web", "httpclient.jl",))
+# include(joinpath("web", "httpserver.jl",))
+# include(joinpath("web", "juliawebapi.jl",))
+# include(joinpath("web", "mux.jl",))
 # include(joinpath("web", "websockets.jl",))
 
 end # end submodule PredictMD.Server
