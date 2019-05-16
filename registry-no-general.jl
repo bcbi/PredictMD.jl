69f1a87903a1cf81752258b0021b7181cf598a41
Dilum Aluthge <dilum@aluthge.com>
2019-05-16 01:41:28 -0400

    Rename PredictMDRegistry to BCBIRegistry

--- registry-no-general.jl
+++ registry-no-general.jl
@@ -1,6 +1,6 @@
 import Pkg; 
 
-Pkg.Registry.add(Pkg.RegistrySpec(name="PredictMDRegistry",url="https://github.com/bcbi/PredictMDRegistry.git",uuid="26a550a3-39fe-4af4-af6d-e8814c2b6dd9",)); 
-Pkg.Registry.update(Pkg.RegistrySpec(name="PredictMDRegistry",uuid="26a550a3-39fe-4af4-af6d-e8814c2b6dd9")); 
+Pkg.Registry.add(Pkg.RegistrySpec(name="BCBIRegistry",url="https://github.com/bcbi/BCBIRegistry.git",uuid="26a550a3-39fe-4af4-af6d-e8814c2b6dd9",)); 
+Pkg.Registry.update(Pkg.RegistrySpec(name="BCBIRegistry",uuid="26a550a3-39fe-4af4-af6d-e8814c2b6dd9")); 
 
-println("The `PredictMDRegistry` registry was installed successfully."); 
+println("The `BCBIRegistry` registry was installed successfully."); 
