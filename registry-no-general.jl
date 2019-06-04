493e09af49301b995c27404ab97ee6f0cff1ab2d
Dilum Aluthge <dilum@aluthge.com>
2019-06-03 22:23:40 -0400

    Delete registry-no-general.jl

--- registry-no-general.jl
+++
@@ -1,6 +0,0 @@
-import Pkg; 
-
-Pkg.Registry.add(Pkg.RegistrySpec(name="BCBIRegistry",url="https://github.com/bcbi/BCBIRegistry.git",uuid="26a550a3-39fe-4af4-af6d-e8814c2b6dd9",)); 
-Pkg.Registry.update(Pkg.RegistrySpec(name="BCBIRegistry",uuid="26a550a3-39fe-4af4-af6d-e8814c2b6dd9")); 
-
-println("The `BCBIRegistry` registry was installed successfully."); 
