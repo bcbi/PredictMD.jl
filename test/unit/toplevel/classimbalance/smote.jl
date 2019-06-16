4189a9833a213032e94f4403c14c572bf8968dad
Dilum Aluthge <dilum@aluthge.com>
2019-06-15 20:51:27 -0400

    Update smote.jl

--- test/unit/toplevel/classimbalance/smote.jl
+++ test/unit/toplevel/classimbalance/smote.jl
@@ -3,16 +3,10 @@ import Test
 
 Test.@test_throws(
     ErrorException,
-    calculate_smote_pct_under(;pct_over=-111,minority_to_majority_ratio=0.5),
+    PredictMD.calculate_smote_pct_under(;pct_over=-111,minority_to_majority_ratio=0.5),
     )
 
 Test.@test_throws(
     ErrorException,
-    calculate_smote_pct_under(;pct_over=100,minority_to_majority_ratio=-1.5),
-    )
-
-
-Test.@test_throws(
-    ErrorException,
-    smote(),
+    PredictMD.calculate_smote_pct_under(;pct_over=100,minority_to_majority_ratio=-1.5),
     )
