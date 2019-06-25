c5ccfa605e386865f10fb2037df113d762e55b76
Dilum Aluthge <dilum@aluthge.com>
2019-06-24 23:07:36 -0400

    Update ccs.jl

--- src/toplevel/ontologies/ccs.jl
+++ src/toplevel/ontologies/ccs.jl
@@ -1,7 +1,7 @@
-_ICD_ICD9_CCS_APPENDIXASINGLEDX_FILE_HAS_BEEN_PARSED = false
-_ICD9_CODE_TO_SINGLE_LEVEL_DX_CCS = Dict{String, Int}()
-_SINGLE_LEVEL_DX_CCS_NUMBER_TO_NAME = Dict{Int, String}()
-_SINGLE_LEVEL_DX_CCS_TO_LIST_OF_ICD9_CODES = Dict{Int, String}()
+const _ICD_ICD9_CCS_APPENDIXASINGLEDX_FILE_HAS_BEEN_PARSED = Ref(false)
+const _ICD9_CODE_TO_SINGLE_LEVEL_DX_CCS = Ref(Dict{String, Int}())
+const _SINGLE_LEVEL_DX_CCS_NUMBER_TO_NAME = Ref(Dict{Int, String}())
+const _SINGLE_LEVEL_DX_CCS_TO_LIST_OF_ICD9_CODES = Ref(Dict{Int, String}())
 
 function remove_all_full_stops(x::AbstractString)::String
     result = replace(x, "." => "")
@@ -9,7 +9,11 @@ function remove_all_full_stops(x::AbstractString)::String
 end
 
 function parse_icd_icd9_ccs_appendixasingledx_file!()::Nothing
-    if _ICD_ICD9_CCS_APPENDIXASINGLEDX_FILE_HAS_BEEN_PARSED
+    global _ICD_ICD9_CCS_APPENDIXASINGLEDX_FILE_HAS_BEEN_PARSED
+    global _ICD9_CODE_TO_SINGLE_LEVEL_DX_CCS
+    global _SINGLE_LEVEL_DX_CCS_NUMBER_TO_NAME
+    global _SINGLE_LEVEL_DX_CCS_TO_LIST_OF_ICD9_CODES
+    if _ICD_ICD9_CCS_APPENDIXASINGLEDX_FILE_HAS_BEEN_PARSED[]
     else
         filename = package_directory(
             "assets",
@@ -28,22 +32,22 @@ function parse_icd_icd9_ccs_appendixasingledx_file!()::Nothing
                 section_pieces = split(section)
                 ccs_number = parse(Int, section_pieces[1])
                 ccs_name = section_pieces[2]
-                _SINGLE_LEVEL_DX_CCS_NUMBER_TO_NAME[ccs_number] =
+                _SINGLE_LEVEL_DX_CCS_NUMBER_TO_NAME[][ccs_number] =
                     ccs_name
                 icd9_code_list = section_pieces[3:end]
                 icd9_code_list = [
                     convert(String, strip(x)) for x in icd9_code_list
                     ]
                 icd9_code_list = convert(Vector{String}, icd9_code_list)
-                _SINGLE_LEVEL_DX_CCS_TO_LIST_OF_ICD9_CODES[ccs_number] =
+                _SINGLE_LEVEL_DX_CCS_TO_LIST_OF_ICD9_CODES[][ccs_number] =
                     icd9_code_list
                 for icd9_code in icd9_code_list
-                    _ICD9_CODE_TO_SINGLE_LEVEL_DX_CCS[icd9_code] =
+                    _ICD9_CODE_TO_SINGLE_LEVEL_DX_CCS[][icd9_code] =
                         ccs_number
                 end
             end
         end
-        _ICD_ICD9_CCS_APPENDIXASINGLEDX_FILE_HAS_BEEN_PARSED = true
+        _ICD_ICD9_CCS_APPENDIXASINGLEDX_FILE_HAS_BEEN_PARSED[] = true
     end
     return nothing
 end
@@ -52,7 +56,8 @@ function single_level_dx_ccs_number_to_name(
         ccs_number::Int,
         )::String
     parse_icd_icd9_ccs_appendixasingledx_file!()
-    result = _SINGLE_LEVEL_DX_CCS_NUMBER_TO_NAME[ccs_number]
+    global _SINGLE_LEVEL_DX_CCS_NUMBER_TO_NAME
+    result = _SINGLE_LEVEL_DX_CCS_NUMBER_TO_NAME[][ccs_number]
     return result
 end
 
@@ -60,7 +65,8 @@ function single_level_dx_ccs_to_list_of_icd9_codes(
         ccs_number::Int,
         )::Vector{String}
     parse_icd_icd9_ccs_appendixasingledx_file!()
-    result = _SINGLE_LEVEL_DX_CCS_TO_LIST_OF_ICD9_CODES[ccs_number]
+    global _SINGLE_LEVEL_DX_CCS_TO_LIST_OF_ICD9_CODES
+    result = _SINGLE_LEVEL_DX_CCS_TO_LIST_OF_ICD9_CODES[][ccs_number]
     return result
 end
 
@@ -70,9 +76,9 @@ function icd9_code_to_single_level_dx_ccs(
         icd9_code::AbstractString,
         )::Int
     parse_icd_icd9_ccs_appendixasingledx_file!()
-    result = _ICD9_CODE_TO_SINGLE_LEVEL_DX_CCS[
+    global _ICD9_CODE_TO_SINGLE_LEVEL_DX_CCS
+    result = _ICD9_CODE_TO_SINGLE_LEVEL_DX_CCS[][
         remove_all_full_stops(string(icd9_code))
         ]
     return result
 end
-
