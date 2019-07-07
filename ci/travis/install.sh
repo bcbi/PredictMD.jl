ef612db04dbdeb8e34c4584dbc9c9918cadf6027
Dilum Aluthge <dilum@aluthge.com>
2019-07-07 17:06:25 -0400

    Update install.sh

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -21,14 +21,15 @@ else
     :
 fi
 
-mkdir -p $HOME/predictmd_cache_travis
-find $HOME/predictmd_cache_travis
-
 export GROUP="$1"
 echo "GROUP=$GROUP"
 
+mkdir -p $HOME/predictmd_cache_travis
+tree $HOME/predictmd_cache_travis
+
 if [[ "$GROUP" == "$FIRST_GROUP" ]];
 then
+    # rm -rf $HOME/predictmd_cache_travis_discard_firststage
     # mv $HOME/predictmd_cache_travis $HOME/predictmd_cache_travis_discard_firststage
     :
 else
@@ -36,7 +37,7 @@ else
 fi
 
 mkdir -p $HOME/predictmd_cache_travis
-find $HOME/predictmd_cache_travis
+tree $HOME/predictmd_cache_travis
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]];
 then
@@ -56,9 +57,10 @@ echo "DO_TESTS=$DO_TESTS"
 
 if [[ "$DO_TESTS" == "true" ]];
 then
+    julia $JULIA_FLAGS -e 'import Pkg;Pkg.Registry.add("General");'
+    julia $JULIA_FLAGS -e 'import Pkg;Pkg.Registry.update();'
     julia $JULIA_FLAGS -e 'import Pkg;Pkg.Registry.add(Pkg.RegistrySpec(name="BCBIRegistry",url="https://github.com/bcbi/BCBIRegistry.git",uuid="26a550a3-39fe-4af4-af6d-e8814c2b6dd9",));'
     julia $JULIA_FLAGS -e 'import Pkg;Pkg.Registry.update();'
-    julia $JULIA_FLAGS -e 'import Pkg;Pkg.Registry.add("General");'
     julia $JULIA_FLAGS -e 'import Pkg;Pkg.build("PredictMD");'
     julia $JULIA_FLAGS -e 'import Pkg;Pkg.test("PredictMD"; coverage=true);'
     if [[ "$GROUP" == "$LAST_GROUP" ]]; then
@@ -76,12 +78,13 @@ else
 fi
 
 mkdir -p $HOME/predictmd_cache_travis
-find $HOME/predictmd_cache_travis
+tree $HOME/predictmd_cache_travis
 
 if [[ "$GROUP" == "$LAST_GROUP" ]]; then
+    # rm $HOME/predictmd_cache_travis_discard_laststage
     # mv $HOME/predictmd_cache_travis $HOME/predictmd_cache_travis_discard_laststage
     :
 fi
 
 mkdir -p $HOME/predictmd_cache_travis
-find $HOME/predictmd_cache_travis
+tree $HOME/predictmd_cache_travis
