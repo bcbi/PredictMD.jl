2d08a3eb068272f18b8815e5142ccd5f7e505364
Dilum Aluthge <dilum@aluthge.com>
2019-07-07 17:01:42 -0400

    Update registry_url_list.jl

--- src/registry_url_list.jl
+++ src/registry_url_list.jl
@@ -1,9 +1,8 @@
-# function registry_url_list()::Vector{String}
-#     registry_url_list_raw::Vector{String} = String[
-#         "https://github.com/JuliaRegistries/General",
-#         "https://github.com/bcbi/BCBIRegistry",
-#         ]
-#     registry_url_list::Vector{String} = strip.(registry_url_list_raw)
-#     return registry_url_list
-# end
-
+function registry_url_list()::Vector{String}
+    registry_url_list_raw::Vector{String} = String[
+        "https://github.com/JuliaRegistries/General.git",
+        "https://github.com/bcbi/BCBIRegistry.git",
+        ]
+    registry_url_list::Vector{String} = strip.(registry_url_list_raw)
+    return registry_url_list
+end
