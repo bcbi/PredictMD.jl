b011bdc21fa9e87993c18176e1b461e1cc905aab
Dilum Aluthge <dilum@aluthge.com>
2019-07-07 13:09:34 -0400

    Various whiteboard improvements

--- src/init.jl
+++ src/init.jl
@@ -22,7 +22,7 @@ function __init__()::Nothing
     Requires.@require MLJ="add582a8-e3ab-11e8-2d5e-e98b27df1bc7" begin
         Requires.@require MLJBase="a7f614a8-145f-11e9-1d2a-a57a1082229d" begin
             Requires.@require MLJModels="d491faf4-2d78-11e9-2867-c94bc002c0b7" begin
-                import PredictMD_Glue_MLJ
+                include("toplevel/conditionally-loaded/mlj/mlj.jl")
             end
         end
     end
---
+++ src/toplevel/conditionally-loaded/mlj/mlj.jl
@@ -0,0 +1,7 @@
+import .MLJ
+import .MLJBase
+import .MLJModels
+
+@info("PredictMD: the following backend is now available: MLJ")
+@info("PredictMD: the following backend is now available: MLJBase")
+@info("PredictMD: the following backend is now available: MLJModels")
