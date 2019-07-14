fdf66a9581719740c858ed425ee78f54d8c728d4
Dilum Aluthge <dilum@aluthge.com>
2019-07-14 01:48:46 -0400

    Progress commit

--- src/init.jl
+++ src/init.jl
@@ -5,7 +5,6 @@ function __init__()::Nothing
         Requires.@require MLJBase="a7f614a8-145f-11e9-1d2a-a57a1082229d" begin
             Requires.@require MLJModels="d491faf4-2d78-11e9-2867-c94bc002c0b7" begin
                 include("toplevel/conditionally-loaded/mlj/mlj.jl")
-                @info("PredictMD: the following backends are now available: MLJ, MLJBase, MLJModels")
             end
         end
     end
--- src/welcome.jl
+++ src/welcome.jl
@@ -1,20 +1,26 @@
 import Pkg
 
-function print_welcome_message()::Nothing
+function print_welcome_message(a::AbstractDict = ENV)::Nothing
     predictmd_version::VersionNumber = version()
     predictmd_version_codename::String = version_codename()
     predictmd_pkgdir::String = package_directory()
     predictmdapi_version::VersionNumber = api_version()
     predictmdapi_pkgdir::String = api_package_directory()
-    @info(
-        string(
-            "This is PredictMD, version $(predictmd_version), ",
-            "code name \"$(predictmd_version_codename)\"",
+    if !_suppress_welcome_message(a)
+        @info(
+            string(
+                "This is PredictMD, version $(predictmd_version), ",
+                "code name \"$(predictmd_version_codename)\"",
+                )
             )
-        )
-    @info("For help, please visit https://predictmd.net")
-    @debug("PredictMD package directory: $(predictmd_pkgdir)")
-    @debug("PredictMDAPI version: $(predictmdapi_version)")
-    @debug("PredictMDAPI package directory: $(predictmdapi_pkgdir)")
+        @info("For help, please visit https://predictmd.net")
+        @debug("PredictMD package directory: $(predictmd_pkgdir)")
+        @debug("PredictMDAPI version: $(predictmdapi_version)")
+        @debug("PredictMDAPI package directory: $(predictmdapi_pkgdir)")
+    end
     return nothing
+end
+
+function _suppress_welcome_message(a::AbstractDict = ENV)::Bool
+    return get(a, "SUPPRESS_PREDICTMD_WELCOME_MESSAGE", "false") == "true"
 end
