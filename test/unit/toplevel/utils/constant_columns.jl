f33520eaffdda07b1d7a7650886410f252408b8a
Dilum Aluthge <dilum@aluthge.com>
2019-08-08 13:56:44 -0400

    Add more constant-column tests

--- test/unit/toplevel/utils/constant_columns.jl
+++ test/unit/toplevel/utils/constant_columns.jl
@@ -1,3 +1,7 @@
+import DataFrames
+import PredictMD
+import Test
+
 x = Union{Missing, String}["foo", "bar", "foo", missing, "bar"]
 
 Test.@test(
@@ -38,3 +42,11 @@ Test.@test(
 Test.@test(
     PredictMD.get_number_of_unique_values_include_missings(x) == 3
     )
+
+df = DataFrames.DataFrame()
+df[:a] = [1,2,3,4,5,6,7]
+df[:b] = [1,1,1,1,1,1,1]
+df_constant_cols = PredictMD.find_constant_columns(df)
+Test.@test length(df_constant_cols) == 1
+Test.@test df_constant_cols == [:b]
+Test.@test all(df_constant_cols .== [:b])
