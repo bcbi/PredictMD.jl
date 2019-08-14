f808656dc2b5f179591c5449c95d2c7de4a3a4ec
Dilum Aluthge <dilum@aluthge.com>
2019-08-13 22:15:51 -0400

    Progress commit

--- ci/travis/compatibility.sh
+++ ci/travis/compatibility.sh
@@ -9,9 +9,9 @@ mkdir -p ~/environments/environment-predictmd-first
 cd ~/environments/environment-predictmd-first
 touch Project.toml
 rm -rf ~/.julia
-julia -e --project 'import Pkg; Pkg.develop(Pkg.PackageSpec(path=ENV["TRAVIS_BUILD_DIR"]))'
-julia -e --project 'import Pkg; Pkg.develop(Pkg.PackageSpec(url="https://github.com/bcbi/PredictMDExtra.jl.git"))'
-julia -e --project 'import Pkg; Pkg.develop(Pkg.PackageSpec(url="https://github.com/bcbi/PredictMDFull.jl.git"))'
+julia --project -e 'import Pkg; Pkg.develop(Pkg.PackageSpec(path=ENV["TRAVIS_BUILD_DIR"]))'
+julia --project -e 'import Pkg; Pkg.develop(Pkg.PackageSpec(url="https://github.com/bcbi/PredictMDExtra.jl.git"))'
+julia --project -e 'import Pkg; Pkg.develop(Pkg.PackageSpec(url="https://github.com/bcbi/PredictMDFull.jl.git"))'
 
 cd ~
 rm -rf ~/.julia
@@ -20,6 +20,6 @@ mkdir -p ~/environments/environment-extra-first
 cd ~/environments/environment-extra-first
 touch Project.toml
 rm -rf ~/.julia
-julia -e --project 'import Pkg; Pkg.develop(Pkg.PackageSpec(url="https://github.com/bcbi/PredictMDExtra.jl.git"))'
-julia -e --project 'import Pkg; Pkg.develop(Pkg.PackageSpec(path=ENV["TRAVIS_BUILD_DIR"]))'
-julia -e --project 'import Pkg; Pkg.develop(Pkg.PackageSpec(url="https://github.com/bcbi/PredictMDFull.jl.git"))'
+julia --project -e 'import Pkg; Pkg.develop(Pkg.PackageSpec(url="https://github.com/bcbi/PredictMDExtra.jl.git"))'
+julia --project -e 'import Pkg; Pkg.develop(Pkg.PackageSpec(path=ENV["TRAVIS_BUILD_DIR"]))'
+julia --project -e 'import Pkg; Pkg.develop(Pkg.PackageSpec(url="https://github.com/bcbi/PredictMDFull.jl.git"))'
