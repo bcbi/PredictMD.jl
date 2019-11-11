9fbe39c0e8f65b234ec5d595f586d81d7b175ae2
Dilum Aluthge <dilum@aluthge.com>
2019-11-10 23:27:53 -0500

    Fix compat entries and run tests on Julia 1.2

--- src/import_all.jl
+++ src/import_all.jl
@@ -1,38 +1,60 @@
+macro import_all()
+    return _import_all_macro()
+end
+
+function _import_all_macro()
+    statements_1 = _import_all_statements()
+    pkg_list::Vector{String} = convert(Vector{String}, sort(unique(strip.(package_list()))))::Vector{String}
+    n = length(pkg_list)
+    statements_2 = Vector{String}(undef, n)
+    for i = 1:n
+        pkgname = pkg_list[i]
+        statements_2[i] = "import $(pkgname)"
+    end
+    statements = vcat(statements_1, statements_2)
+    ex = Base.Meta.parse(join(statements, "; "))
+    return ex
+end
+
+function _import_all_statements()::Vector{String}
+    statements = Vector{String}(undef, 0)
+    _import_all_statements!(statements)
+    return statements
+end
+
+function _import_all_statements!(statements::Vector{String})::Vector{String}
+    pkg_list::Vector{String} = convert(Vector{String}, sort(unique(strip.(package_list()))))::Vector{String}
+    for p in pkg_list
+        a = string("try ",
+                   "    import $(string(p)); ",
+                   "catch e1 ",
+                   "    @debug(\"ignoring exception: \", e1,); ",
+                   "    try ",
+                   "        import Pkg; ",
+                   "        Pkg.add(\"$(string(p))\"); ",
+                   "    catch e2 ",
+                   "        @debug(\"ignoring exception: \", e2,); ",
+                   "    end ",
+                   "end ")
+        b = string("try ",
+                   "    import $(string(p)); ",
+                   "    @debug(\"imported $(string(p))\"); ",
+                   "catch e3 ",
+                   "    @error(\"ignoring exception: \", exception=e3,); ",
+                   "end ")
+        push!(statements, a)
+        push!(statements, b)
+    end
+    return statements
+end
+
 import_all() = import_all(Main)
 
 function import_all(m::Module)::Nothing
-    pkg_list::Vector{String} = sort(unique(strip.(package_list())))
-    for p in pkg_list
-        Base.eval(
-            m,
-            Base.Meta.parse(
-                string(
-                    "try ",
-                    "import $(string(p)); ",
-                    "catch e1 ",
-                    "@debug(\"ignoring exception: \", e1,); ",
-                    "try ",
-                    "import Pkg; ",
-                    "Pkg.add(\"$(string(p))\"); ",
-                    "catch e2 ",
-                    "@debug(\"ignoring exception: \", e2,); ",
-                    "end ",
-                    "end ",
-                    ),
-                ),
-            )
-        Base.eval(
-            m,
-            Base.Meta.parse(
-                string(
-                    "try ",
-                    "import $(string(p)); ",
-                    "@debug(\"imported $(string(p))\"); ",
-                    "catch e3 ",
-                    "@error(\"ignoring exception: \", exception=e3,); ",
-                    "end ",
-                    ),
-                ),
-            )
+    statements = _import_all_statements()
+    for stmt in statements
+        ex = Base.Meta.parse(stmt)
+        Base.eval(m, ex)
     end
+    return nothing
 end
